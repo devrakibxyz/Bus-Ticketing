597dde4656a0717678e3e68167b07e80526e80b4
Rakib <iamrakib.ita@gmail.com>
2024-02-19 00:30:47 +0100

    updated

--- app.js
+++ app.js
@@ -5,20 +5,18 @@ let count2 = 0;
 
 for (const btn of allBtn) {
   btn.addEventListener("click", function (e) {
-    e.target.style.backgroundColor = "#1dd100";
-    e.target.style.color = "white";
-
-    const seatContainer = document.getElementById("seat");
     const seatCount = document.getElementById("seat-count");
     seatCount.classList.remove("hidden");
     count2 += 1;
     seatCount.innerText = count2;
 
-    // seatCount.innerText = seatContainer.childElementCount;
-    // const seatcountt = seatCount.innerText;
-
+    const seatContainer = document.getElementById("seat");
     if (seatContainer.childElementCount > 3) {
       return;
+    }
+    if (seatContainer.childElementCount < 4) {
+      e.target.style.backgroundColor = "#1dd100";
+      e.target.style.color = "white";
     }
 
     count -= 1;
@@ -42,10 +40,18 @@ for (const btn of allBtn) {
 
     const totalPrice = document.getElementById("total-price").innerText;
     const convertPrice = parseInt(totalPrice);
-    console.log(convertPrice);
 
-    const sum = convertPrice + parseInt(ticketPrice);
-    setInnerText("total-price", sum);
+    const totalTicketPrice = convertPrice + parseInt(ticketPrice);
+    setInnerText("total-price", totalTicketPrice);
+    setInnerText("grand-total", totalTicketPrice);
+    const discount = totalTicketPrice * 0.15;
+    const grand = document.getElementById("grand-total");
+
+    document
+      .getElementById("coupon-button")
+      .addEventListener("click", function () {
+        grand.innerText = totalTicketPrice - discount;
+      });
   });
 }
 
@@ -57,7 +63,7 @@ document.getElementById("input-text").addEventListener("keyup", function (e) {
   const deleteButton = document.getElementById("coupon-button");
   if (text == "NEW15") {
     deleteButton.removeAttribute("disabled");
-  } else if (text == "couple 20") {
+  } else if (text == "Couple 20") {
     deleteButton.removeAttribute("disabled");
   } else {
     deleteButton.setAttribute("disabled", true);
